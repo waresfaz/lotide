a56634d31b6e8884b302677dcf4fbb37c02a8d51
Wares <fazelyar.wares@gmail.com>
2020-02-05 18:45:36 +0000

    copmleted eqArrays.js and started assertArrayEquals.js

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i <= arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      } 
+    } return true; // if this was line above, after  first iteration it would return true and stop looping. Must be outside of if statement block to loop to end.
+  }
+}
+
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
