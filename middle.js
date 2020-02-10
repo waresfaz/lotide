b77121a6356b6bcbc56558aa4e1b286cac72c778
Wares <fazelyar.wares@gmail.com>
2020-02-10 18:59:58 +0000

    refactored

--- middle.js
+++ middle.js
@@ -1,22 +1,5 @@
-const eqArrays = function(arrOne, arrTwo) {
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  } else {
-    for (let i = 0; i <= arrOne.length; i++) {
-      if (arrOne[i] !== arrTwo[i]) {
-        return false;
-      } 
-    } return true; 
-  }
-}
-
-const assertArraysEqual = function(firstArray, secondArray) {
-  if (eqArrays(firstArray, secondArray)) {
-    console.log(`✅ Assertion passed: ${firstArray} === ${secondArray}`);
-  } else {
-    console.log(`🛑 Assertion failed: ${firstArray} !== ${secondArray}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
   if (array.length === 0 || array.length === 1 || array.length === 2) {
@@ -28,6 +11,5 @@ const middle = function(array) {
   }
 } 
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,5 @@
+const middle = require('../middle');
+
+console.log(middle([1])); // => []
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
